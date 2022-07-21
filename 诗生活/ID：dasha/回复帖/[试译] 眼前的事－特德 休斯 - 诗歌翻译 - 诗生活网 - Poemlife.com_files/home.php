b8f8d018da2be1801929e447f139a4ec6b91da9b
vivo <mahavivo@126.com>
2022-07-21 15:44:38 +0800

    edit

---
+++ 诗生活/ID：dasha/回复帖/[试译] 眼前的事－特德 休斯 - 诗歌翻译 - 诗生活网 - Poemlife.com_files/home.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>提示信息 -  诗生活网 -  Poemlife.com</title>
+
+<meta name="keywords" content="" />
+<meta name="description" content=",诗生活网" />
+<meta name="generator" content="Discuz! X3.4" />
+<meta name="author" content="Discuz! Team and Comsenz UI Team" />
+<meta name="copyright" content="2001-2021 Tencent Cloud." />
+<meta name="MSSmartTagsPreventParsing" content="True" />
+<meta http-equiv="MSThemeCompatible" content="Yes" />
+<base href="https://www.poemlife.com/" /><link rel="stylesheet" type="text/css" href="data/cache/style_1_common.css?C77" /><link rel="stylesheet" type="text/css" href="data/cache/style_1_home_spacecp.css?C77" /><link rel="stylesheet" id="css_extstyle" type="text/css" href="./template/default/style/t2/style.css" /><script type="text/javascript">var STYLEID = '1', STATICURL = 'static/', IMGDIR = 'static/image/common', VERHASH = 'C77', charset = 'utf-8', discuz_uid = '0', cookiepre = 'poemlifebbs_2132_', cookiedomain = '', cookiepath = '/', showusercard = '0', attackevasive = '0', disallowfloat = 'login|sendpm|newthread|reply|viewratings|viewwarning|viewthreadmod|viewvote|tradeorder|activity|debate|nav|usergroups|task', creditnotice = '', defaultstyle = './template/default/style/t2', REPORTURL = 'aHR0cHM6Ly93d3cucG9lbWxpZmUuY29tL2hvbWUucGhwP21vZD1zcGFjZWNwJmFjPXBtJm9wPWNoZWNrbmV3cG0mcmFuZD0xNjU4MzgwMjcx', SITEURL = 'https://www.poemlife.com/', JSPATH = 'static/js/', CSSPATH = 'data/cache/style_', DYNAMICURL = '';</script>
+<script src="static/js/common.js?C77" type="text/javascript"></script>
+<meta name="application-name" content="诗生活网" />
+<meta name="msapplication-tooltip" content="诗生活网" />
+<meta name="msapplication-task" content="name=诗歌论坛;action-uri=https://www.poemlife.com/forum.php;icon-uri=https://www.poemlife.com/static/image/common/bbs.ico" />
+<script src="static/js/home.js?C77" type="text/javascript"></script>
+</head>
+
+<body id="nv_home" class="pg_spacecp" onkeydown="if(event.keyCode==27) return false;">
+<div id="append_parent"></div><div id="ajaxwaitid"></div>
+<div id="toptb" class="cl">
+<div class="wp">
+<div class="z"><a href="javascript:;"  onclick="setHomepage('https://www.poemlife.com/');">设为首页</a><a href="https://www.poemlife.com/"  onclick="addFavorite(this.href, '诗生活网');return false;">收藏本站</a></div>
+<div class="y">
+<a id="switchblind" href="javascript:;" onclick="toggleBlind(this)" title="开启辅助访问" class="switchblind"></a>
+<a href="javascript:;" id="switchwidth" onclick="widthauto(this)" title="切换到宽版" class="switchwidth">切换到宽版</a>
+</div>
+</div>
+</div>
+
+<div id="qmenu_menu" class="p_pop blk" style="display: none;">
+<div class="ptm pbw hm">
+请 <a href="javascript:;" class="xi2" onclick="lsSubmit()"><strong>登录</strong></a> 后使用快捷导航<br />没有账号？<a href="member.php?mod=zcssh123" class="xi2 xw1">立即注册</a>
+</div>
+<div id="fjump_menu" class="btda"></div></div><div id="hd">
+<div class="wp">
+<div class="hdc cl"><h2><a href="./" title="诗生活网"><img src="static/image/common/logo.png" alt="诗生活网" border="0" /></a></h2><script src="static/js/logging.js?C77" type="text/javascript"></script>
+<form method="post" autocomplete="off" id="lsform" action="member.php?mod=logging&amp;action=login&amp;loginsubmit=yes&amp;infloat=yes&amp;lssubmit=yes" onsubmit="pwmd5('ls_password');return lsSubmit();">
+<div class="fastlg cl">
+<span id="return_ls" style="display:none"></span>
+<div class="y pns">
+<table cellspacing="0" cellpadding="0">
+<tr>
+<td><label for="ls_username">账号</label></td>
+<td><input type="text" name="username" id="ls_username" class="px vm xg1"  value="UID/用户名/Email" onfocus="if(this.value == 'UID/用户名/Email'){this.value = '';this.className = 'px vm';}" onblur="if(this.value == ''){this.value = 'UID/用户名/Email';this.className = 'px vm xg1';}" tabindex="901" /></td>
+<td class="fastlg_l"><label for="ls_cookietime"><input type="checkbox" name="cookietime" id="ls_cookietime" class="pc" value="2592000" tabindex="903" />自动登录</label></td>
+<td>&nbsp;<a href="javascript:;" onclick="showWindow('login', 'member.php?mod=logging&action=login&viewlostpw=1')">找回密码</a></td>
+</tr>
+<tr>
+<td><label for="ls_password">密码</label></td>
+<td><input type="password" name="password" id="ls_password" class="px vm" autocomplete="off" tabindex="902" /></td>
+<td class="fastlg_l"><button type="submit" class="pn vm" tabindex="904" style="width: 75px;"><em>登录</em></button></td>
+<td>&nbsp;<a href="member.php?mod=zcssh123" class="xi2 xw1">立即注册</a></td>
+</tr>
+</table>
+<input type="hidden" name="formhash" value="be26dbde" />
+<input type="hidden" name="quickforward" value="yes" />
+<input type="hidden" name="handlekey" value="ls" />
+</div>
+</div>
+</form>
+
+<script src="static/js/md5.js?C77" type="text/javascript" reload="1"></script>
+</div>
+
+<div id="nv">
+<a href="javascript:;" id="qmenu" onmouseover="delayShow(this, function () {showMenu({'ctrlid':'qmenu','pos':'34!','ctrlclass':'a','duration':2});showForummenu(0);})">快捷导航</a>
+<ul><li id="mn_Nf036" ><a href="http://www.poemlife.com/" hidefocus="true"  >首页</a></li><li id="mn_forum" ><a href="forum.php" hidefocus="true" title="BBS"  >诗歌论坛<span>BBS</span></a></li><li id="mn_N75c5" ><a href="forum.php?gid=123" hidefocus="true"  >友情论坛</a></li><li id="mn_Nf6c6" ><a href="home.php?mod=spacecp&ac=invite" hidefocus="true"  >邀请好友</a></li></ul>
+</div>
+<div class="p_pop h_pop" id="mn_userapp_menu" style="display: none"></div><div id="mu" class="cl">
+</div><div id="scbar" class="cl">
+<form id="scbar_form" method="post" autocomplete="off" onsubmit="searchFocus($('scbar_txt'))" action="search.php?searchsubmit=yes" target="_blank">
+<input type="hidden" name="mod" id="scbar_mod" value="search" />
+<input type="hidden" name="formhash" value="be26dbde" />
+<input type="hidden" name="srchtype" value="title" />
+<input type="hidden" name="srhfid" value="0" />
+<input type="hidden" name="srhlocality" value="home::spacecp" />
+<table cellspacing="0" cellpadding="0">
+<tr>
+<td class="scbar_icon_td"></td>
+<td class="scbar_txt_td"><input type="text" name="srchtxt" id="scbar_txt" value="请输入搜索内容" autocomplete="off" x-webkit-speech speech /></td>
+<td class="scbar_type_td"><a href="javascript:;" id="scbar_type" class="xg1" onclick="showMenu(this.id)" hidefocus="true">搜索</a></td>
+<td class="scbar_btn_td"><button type="submit" name="searchsubmit" id="scbar_btn" sc="1" class="pn pnc" value="true"><strong class="xi2">搜索</strong></button></td>
+<td class="scbar_hot_td">
+<div id="scbar_hot">
+</div>
+</td>
+</tr>
+</table>
+</form>
+</div>
+<ul id="scbar_type_menu" class="p_pop" style="display: none;"><li><a href="javascript:;" rel="user">用户</a></li></ul>
+<script type="text/javascript">
+initSearchmenu('scbar', '');
+</script>
+</div>
+</div>
+
+
+<div id="wp" class="wp">
+<div id="ct" class="wp cl w">
+<div class="nfl" id="main_succeed" style="display: none">
+<div class="f_c altw">
+<div class="alert_right">
+<p id="succeedmessage"></p>
+<p id="succeedlocation" class="alert_btnleft"></p>
+<p class="alert_btnleft"><a id="succeedmessage_href">如果您的浏览器没有自动跳转，请点击此链接</a></p>
+</div>
+</div>
+</div>
+<div class="nfl" id="main_message">
+<div class="f_c altw">
+<div id="messagetext" class="alert_info">
+<p>您需要先登录才能继续本操作</p>
+</div>
+<div id="messagelogin"></div>
+<script type="text/javascript">ajaxget('member.php?mod=logging&action=login&infloat=yes&frommessage', 'messagelogin');</script>
+</div>
+</div>
+</div>	</div>
+<div id="ft" class="wp cl">
+<div id="flk" class="y">
+<p>
+<a href="forum.php?mobile=yes" >手机版</a><span class="pipe">|</span><strong><a href="" target="_blank">诗生活网</a></strong>
+( <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备18148997号</a> )</p>
+<p class="xs0">
+GMT+8, 2022-7-21 13:11<span id="debuginfo">
+, Processed in 0.016369 second(s), 0 queries
+, Gzip On, File On.
+</span>
+</p>
+</div>
+<div id="frt">
+<p>Powered by <strong><a href="http://www.discuz.net" target="_blank">Discuz!</a></strong> <em>X3.4</em></p>
+<p class="xs0">Copyright &copy; 2001-2021, Tencent Cloud.</p>
+</div></div>
+<script src="home.php?mod=misc&ac=sendmail&rand=1658380282" type="text/javascript"></script>
+
+<div id="scrolltop">
+<span hidefocus="true"><a title="返回顶部" onclick="window.scrollTo('0','0')" class="scrolltopa" ><b>返回顶部</b></a></span>
+</div>
+<script type="text/javascript">_attachEvent(window, 'scroll', function () { showTopLink(); });checkBlind();</script>
+</body>
+</html>
